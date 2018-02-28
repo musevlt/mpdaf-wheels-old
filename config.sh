e91a0aec8623a8a90b290f588bbc55bbbe72b787
Simon Conseil <contact@saimon.org>
2018-02-28 17:08:04 +0100

    Configure builds

--- config.sh
+++ config.sh
@@ -4,11 +4,17 @@
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
-    :
+    install_pkg_config
+    build_simple cfitsio ${CFITSIO_VERSION:3370} https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/
 }
 
 function run_tests {
     # Runs tests on installed distribution from an empty directory
+    MPDAF_INSTALL_DIR=$(dirname $(python -c 'import mpdaf; print(mpdaf.__file__)'))
+    echo $MPDAF_INSTALL_DIR
     python --version
-    python -c 'import sys; import yourpackage; sys.exit(yourpackage.test())'
+    python -c "import mpdaf; print(mpdaf)"
+    pwd
+    ls
+    pytest $MPDAF_INSTALL_DIR
 }
